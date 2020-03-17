41ac56ba2164d3ec77c184de210fe8c061aee1e9
8c6t <nkw8620@gmail.com>
2020-03-18 00:37:17 +0900

    fix: Account domain field
    password 필드 추가

--- src/main/java/com/hachicore/domain/Account.java
+++ src/main/java/com/hachicore/domain/Account.java
@@ -19,6 +19,8 @@ public class Account {
     @Column(unique = true)
     private String nickname;
 
+    private String password;
+
     private boolean emailVerified;
 
     private String emailCheckToken;
