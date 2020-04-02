a5c4b031bbaaaf6e459046e977aaea84020713c6
8c6t <nkw8620@gmail.com>
2020-04-02 21:06:48 +0900

    2-10. 패스워드를 잊어버렸습니다

--- src/main/java/com/hachicore/config/SecurityConfig.java
+++ src/main/java/com/hachicore/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", "/login", "/sign-up", "/check-email-token", "/email-login", "/check-email-login", "/login-link")
+                .mvcMatchers("/", "/login", "/sign-up", "/check-email-token", "/email-login", "/check-email-login", "/login-link", "/login-by-email")
                     .permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*")
                     .permitAll()
