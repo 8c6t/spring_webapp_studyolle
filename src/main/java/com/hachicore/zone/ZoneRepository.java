196d2708e8e72978a93753bfd25c2e3e668736a4
8c6t <nkw8620@gmail.com>
2020-05-03 20:58:50 +0900

    3-9. 지역 도메인

---
+++ src/main/java/com/hachicore/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.hachicore.zone;
+
+import com.hachicore.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
